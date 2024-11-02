0453dd75e13d08076177b3983260e7c91979cf72
juanadls <eleo.dev09@gmail.com>
2024-11-02 10:18:34 -0300

    break and continue usage examples

--- lib/src/main/java/com/example/lib/MyClass.java
+++ lib/src/main/java/com/example/lib/MyClass.java
@@ -84,6 +84,22 @@ public class MyClass {
 
         }
 
+        for (int i = 0; i  <= 10; i++ ){
+            if (i == 4){
+                break;
+            }
+
+            System.out.println(i);
+        }
+
+        for (int i = 0; i  <= 10; i++ ){
+            if (i == 4){
+                continue;
+            }
+
+            System.out.println(i);
+        }
+
     }
 }
 
