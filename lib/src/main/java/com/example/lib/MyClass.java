c43947f1e3b5b96da51232969e44f9ce0d6264b8
juanadls <eleo.dev09@gmail.com>
2024-11-02 07:28:05 -0300

    setup created

---
+++ lib/src/main/java/com/example/lib/MyClass.java
@@ -0,0 +1,12 @@
+package com.example.lib;
+
+public class MyClass {
+
+    public static void main (String[] args){
+        //Numbers
+        int age = 25;
+        byte x = 103;
+        short y = 7777;
+        long a = 4076509;
+    }
+}
