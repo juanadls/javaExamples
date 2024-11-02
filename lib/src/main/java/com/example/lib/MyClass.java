5f046dc622d86b7fe35cfe0cdfdde9e1dd90f403
juanadls <eleo.dev09@gmail.com>
2024-11-02 07:58:10 -0300

    primitive data types

--- lib/src/main/java/com/example/lib/MyClass.java
+++ lib/src/main/java/com/example/lib/MyClass.java
@@ -3,10 +3,24 @@ package com.example.lib;
 public class MyClass {
 
     public static void main (String[] args){
-        //Numbers
+        //Primitive data types
         int age = 25;
         byte x = 103;
         short y = 7777;
         long a = 4076509;
+        float pi= 3.14F;
+        double b = 2.6;
+        boolean yes = true;
+        char carachter = 'a';
+        char symbol = '@';
+
+        //Explicit casting
+        double pi2 = 3.14;
+        int numPi = (int) pi2;
+
+        //Casting between different data types
+
+        int myInt = 5;
+        char myChar = (char) (myInt + 'a');
     }
 }
