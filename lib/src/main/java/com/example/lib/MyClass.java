5699d5c7e47f255f3b85bee4280e721b3ac8071c
juanadls <eleo.dev09@gmail.com>
2024-11-02 09:52:27 -0300

    conditional statements

--- lib/src/main/java/com/example/lib/MyClass.java
+++ lib/src/main/java/com/example/lib/MyClass.java
@@ -23,13 +23,11 @@ public class MyClass {
         int myInt = 5;
         char myChar = (char) (myInt + 'a');
 
-
         //Arithmetic Operators + - * / %(modulus operator, gives you the leftover of the operation)
 
         //Comparison operators == != > < >= =<
 
         //Logic operators && || !
-
 
         //Strings are objets
         String sayHello = "Hello";
@@ -39,6 +37,33 @@ public class MyClass {
         String myFriends = "My friends";
         System.out.println( sayHello + myFriends);
 
+        //Conditionals if statement
+        int ageM = 18;
+        if (ageM == 18){
+            System.out.println("You are an adult");
+        } else if (ageM == 60) {
+            System.out.println("You are an aged person");
+        } else {
+            System.out.println("You are a minor");
+        }
+
+
+        //Conditional switch statement
+        int dayOfWeek = 3;
+
+        switch (dayOfWeek){
+            case 1:
+                System.out.println("Sunday");
+                break;
+            case 2:
+                System.out.println("Monday");
+                break;
+            case 3:
+                System.out.println("Tuesday");
+                break;
+            default:
+                System.out.println("Default value");
+        }
     }
 }
 
